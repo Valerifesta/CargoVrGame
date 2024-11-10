d7972c1b460daad710b732a595f4761622e8316b
Valerie <victorarsta@gmail.com>
2024-11-11 00:18:18 +0100

    Commented code
    Sorry for spamming commits, i'm just a bit proud about this sytem lol

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -41,20 +41,19 @@ public class MapManager : MonoBehaviour
         MapTexture = new Texture2D(width, height);
 
 
-       // InvokeRepeating("resetTexture", 0, 0.3f);
+       
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
+        /* This is from the learning tests.
+         * 
         float t = Mathf.Sin(Time.time) + 1;
         redSize = Mathf.RoundToInt(Mathf.LerpUnclamped(1, 50, t));
         DotTest();
         */
-
-
 
         if (goal != null)
         {
@@ -65,11 +64,10 @@ public class MapManager : MonoBehaviour
     
     public void AddDrawPending(Color32 color, int size, Vector3 markerWorldPos)
     {
-        
-
         Vector2 dir2D = new Vector2(markerWorldPos.x - _boatScript.transform.position.x, markerWorldPos.z - _boatScript.transform.position.z).normalized;
         float distance = Vector3.Distance(markerWorldPos, _boatScript.transform.position);
 
+        //this scale number is arbitrary but 20 kinda works lol. In a way i guess it kinda becomes the size/bounds of the map
         int offsetScale = 20;
         Color32[] colors = new Color32[size * size];
         for (int i = 0; i < colors.Length; i++)
@@ -77,45 +75,45 @@ public class MapManager : MonoBehaviour
             colors[i] = color;
         }
 
+        //Defines offset of map interest relative to the boat/map center
         offsetX = (int)(dir2D.x * distance * offsetScale);
         offsetY = (int)(dir2D.y * distance * offsetScale);
 
+        //Mashes it all together, the map interest position will coorespond to its world position relative to the boat/map center
         int x = (int)textureCenter().x - (size / 2) + offsetX;
         int y = (int)textureCenter().y - (size / 2) + offsetY;
 
+        //Makes map interests unable to reach beyond the pixel bounds.
         int xClamped = Mathf.Clamp(x, 0, width - size);
         int yClamped = Mathf.Clamp(y, 0, height - size);
         MapTexture.SetPixels32(xClamped, yClamped, size, size, colors);
-        
-
-
     }
 
     public void LateUpdate()
     {
         DrawPending();
-        resetTexture();
+        pendTextureReset();
     }
-    void DrawPending()
+    void DrawPending() //Fulfills the pending changes to the map. the pending is real.
     {
         MapScreenObject.GetComponent<Renderer>().material.mainTexture = MapTexture;
         MapTexture.Apply();
 
     }
     
-    Vector2 textureCenter()
+    Vector2 textureCenter() //Returns center of texture, duh.
     {
         return new Vector2(width / 2, height / 2);
     }
 
-    void resetTexture()
+    void pendTextureReset() //Adds a texture reset to the maps "pending" changes. 
     {
         MapTexture.SetPixels32(defaultColor);
     }
-    void DotTest()
+    void DotTest() //Stuff i messed around with when learning to mannipulate Texture2D properties.
     {
 
-        resetTexture();
+        pendTextureReset();
         //MapScreenObject.GetComponent<Renderer>().material.mainTexture = MapTexture;
 
         Color32[] colors = new Color32[redSize * redSize];
--- Assets/Scripts/MapObjects/Map_Creature.cs
+++ Assets/Scripts/MapObjects/Map_Creature.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 public class Map_Creature : MapInterest
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public override void Start()
     {
-        
+        base.Start();
+        colorRepresent = new Color32(255, 0, 0, 255);
+        colorMarkerSize = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mapMan.AddDrawPending(colorRepresent, colorMarkerSize, transform.position);
     }
 }
